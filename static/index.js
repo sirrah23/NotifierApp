7b29e228253776875a99a306600e2f40d2c70c81
sirrah23 <hseth93@gmail.com>
2018-03-27 00:34:38 -0400

    Created a NotificationTracker class to hold notification logic

--- static/index.js
+++ static/index.js
@@ -1,3 +1,54 @@
+class NotificationTracker{
+    constructor(name, time){
+        this.name = name;
+        this.current_time = time;
+        this.original_time = time;
+        this.state = State.RUNNING;
+        this.looping = false;
+    }
+
+    isRunning(){
+        return this.state === State.RUNNING;
+    }
+
+    isStopped(){
+        return this.state === State.STOPPED;
+    }
+
+    toggleState(){
+        if(this.state === State.RUNNING){
+            this.state = State.STOPPED;
+        } else {
+            this.state = State.RUNNING;
+        }
+    }
+
+    decrementTime(){
+      if(this.isRunning() && this.current_time > 0) this.current_time--;
+    }
+
+    justFinished(){
+        return this.current_time === 0 && this.isRunning()
+    }
+
+    resetTimer(){
+      this.current_time = this.original_time;
+    }
+
+    restartTimer(){
+      if(this.isStopped()) this.toggleState();
+      this.resetTimer();
+    }
+
+    restartOrContinueTimer(){
+      if(this.isStopped() && this.current_time > 0)
+        this.toggleState()
+      else
+        this.restartTimer();
+    }
+
+}
+
 const State = {
     RUNNING: 0,
     STOPPED: 1
@@ -12,23 +63,8 @@ const app = new Vue({
 	},
 	methods: {
 		addNotification(){
-			if(!this.validUserNotifText) return;
-			this.notifications = this.notifications.concat({ //TODO: Make this into an actual object
-				name: this.notification_name, 
-				current_time: this.notification_time,
-				original_time: this.notification_time,
-				state: State.RUNNING,
-                looping: false,
-                isRunning: function(){return this.state === State.RUNNING},
-                isStopped: function(){return this.state === State.STOPPED},
-                toggleState: function(){
-                    if(this.state === State.RUNNING){
-                        this.state = State.STOPPED;
-                    } else{
-                        this.state = State.RUNNING;
-                    }
-                } 
-			});
+			if(!this.validUserNotifText()) return;
+      this.notifications = this.notifications.concat(new NotificationTracker(this.notification_name, this.notification_time));
 			this.clearUserNotifText();
 		},
 		validUserNotifText(){
@@ -39,27 +75,11 @@ const app = new Vue({
 			this.notification_time = "";
 		},
 		decrement_times(){
-            //TODO: Move this logic into notification object
-			this.notifications = this.notifications.map((notification) => {
-				let res;
-				if(notification.isRunning() && notification.current_time !== 0){
-					res = Object.assign(
-						{},
-						notification,
-						{"current_time": notification.current_time-1}
-					);
-				} else {
-					res = Object.assign(
-						{},
-						notification
-					);
-				}
-				return res;
-			});
+		    this.notifications.map(notification => {notification.decrementTime()});
 		},
 		notify(){
 			for(let i = 0; i < this.notifications.length; i++){
-				if(this.notifications[i].current_time === 0 && this.notifications[i].isRunning()){
+				if(this.notifications[i].justFinished()){
 					notifier.notify("Notifier App", this.notifications[i].name);
 					this.notifications[i].toggleState();
 				}
@@ -113,6 +133,7 @@ const Notifier = (function(Notification){
 const notifier = new Notifier();
 notifier.requestDesktopNotificationPermission();
 
+//TODO: Put this random code in the *mounted* lifecycle
 setInterval(() => {
 	app.decrement_times();
 	app.notify();
